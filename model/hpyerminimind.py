bb1adaae1f9289c6935f64304123daa5af183630
Liyining-ai <161810120+Liyining-ai@users.noreply.github.com>
2025-07-25 07:04:25 +0800

    Update hpyerminimind.py

--- model/hpyerminimind.py
+++ model/hpyerminimind.py
@@ -333,7 +333,26 @@ class MOEFeedForward(nn.Module):
 
         return expert_cache
 
-
+def timestep_embedding(t, dim, max_period=10000):
+        """
+        Create sinusoidal timestep embeddings.
+        :param t: a 1-D Tensor of N indices, one per batch element.
+                          These may be fractional.
+        :param dim: the dimension of the output.
+        :param max_period: controls the minimum frequency of the embeddings.
+        :return: an (N, D) Tensor of positional embeddings.
+        """
+        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
+        half = dim // 2
+        freqs = torch.exp(-math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half).to(
+            device=t.device
+        )
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if dim % 2:
+            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+        return embedding
+    
 class MiniMindBlock(nn.Module):
     def __init__(self, layer_id: int, config: MiniMindConfig):
         super().__init__()
@@ -400,7 +419,7 @@ class HyperMiniMindModel(nn.Module):
         for layer_idx in range(self.config.num_hidden_layers):
             # Generate weights from hypernetwork
             t_idx = torch.full((1,), layer_idx, device=input_ids.device)
-            t_embedding = TimestepEmbedder.timestep_embedding(t_idx, 256)
+            t_embedding = timestep_embedding(t_idx, 256)
             flat_w = self.hypernet(t_embedding).view(-1)
 
             # Load weights into shared block
@@ -443,46 +462,6 @@ class TransformerBlockHyperNet(nn.Module):
         # t = t.float().unsqueeze(-1)  # 转为 [batch,1]
         return self.net(t)
         
-class TimestepEmbedder(nn.Module):
-    """
-    Embeds scalar timesteps into vector representations.
-    """
-
-    def __init__(self, hidden_size, frequency_embedding_size=256):
-        super().__init__()
-        self.mlp = nn.Sequential(
-            nn.Linear(frequency_embedding_size, hidden_size, bias=True),
-            nn.SiLU(),
-            nn.Linear(hidden_size, hidden_size, bias=True),
-        )
-        self.frequency_embedding_size = frequency_embedding_size
-
-    @staticmethod
-    def timestep_embedding(t, dim, max_period=10000):
-        """
-        Create sinusoidal timestep embeddings.
-        :param t: a 1-D Tensor of N indices, one per batch element.
-                          These may be fractional.
-        :param dim: the dimension of the output.
-        :param max_period: controls the minimum frequency of the embeddings.
-        :return: an (N, D) Tensor of positional embeddings.
-        """
-        # https://github.com/openai/glide-text2im/blob/main/glide_text2im/nn.py
-        half = dim // 2
-        freqs = torch.exp(-math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half).to(
-            device=t.device
-        )
-        args = t[:, None].float() * freqs[None]
-        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-        if dim % 2:
-            embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
-        return embedding
-
-    def forward(self, t):
-        t_freq = self.timestep_embedding(t, self.frequency_embedding_size)
-        t_emb = self.mlp(t_freq)
-        return t_emb
-
 
                     
 class MiniMindForCausalLM(PreTrainedModel, GenerationMixin):
